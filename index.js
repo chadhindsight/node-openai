23983df408fd48e59042e9b66bf0713852f81814
chad hinds <chadrickj8@gmail.com>
2023-06-06 13:00:17 -0400

    Get the poem from response

--- index.js
+++ index.js
@@ -4,4 +4,17 @@ const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
     apiKey: process.env.OpenAIApi
-})
+});
+
+const openai = new OpenAIApi(configuration);
+
+// Make request
+async function getPoem() {
+    const response = await openai.createChatCompletion({
+        model: "gpt-4",
+        messages: [{ role: "user", content: "generate a short poem about Diablo 4" }]
+    })
+    // get the poem from response
+    const poem = response.data.choices[0].message.content
+    console.log(poem)
+}
